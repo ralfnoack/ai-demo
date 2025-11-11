b1986b25af8aec2fc79ef9e72b282dc30352c6c1
Ralf Noack <224026092+r-noack_uics@users.noreply.github.com>
2025-11-11 10:57:27 +0100

    Rector Auto Optimize applied changes 99

--- rector.php
+++ rector.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 const TYPE_COVERAGE_LEVEL = 26;
 const DEAD_CODE_LEVEL = 0;
-const CODE_QUALITY_LEVEL = 98;
+const CODE_QUALITY_LEVEL = 99;
 
 use Rector\Config\RectorConfig;
 
