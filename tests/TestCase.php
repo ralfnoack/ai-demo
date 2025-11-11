97989238e8535cc9473b5abcb378ca919073e566
Ralf Noack <224026092+r-noack_uics@users.noreply.github.com>
2025-11-11 10:42:18 +0100

    Add initial TestCase class for unit testing setup

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseTestCase;
+
+// use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+}
